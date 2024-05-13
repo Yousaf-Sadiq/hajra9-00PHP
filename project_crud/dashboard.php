b478f24e5e94d60c41affef46bc09aef0258cde2
Yousaf-Sadiq <yousafsadiq445@gmail.com>
2024-05-13 21:53:05 +0500

    php prj

---
+++ project_crud/dashboard.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__)."/layout/user/header.php";
+
+
+echo DASHBOARD;
+?>
+
+
+
+
+<?php
+
+require_once dirname(__FILE__)."/layout/user/footer.php";
+
+?>
---
+++ project_crud/include/web.php
@@ -0,0 +1,17 @@
+<!-- routes  -->
+
+<?php 
+
+// http://localhost/900_HAjra_%20php/project_crud/
+define("SERVER","http://localhost/");
+
+define("FOLDER","900_HAjra_php/project_crud");
+
+
+define("domain1",SERVER.FOLDER);
+
+
+define("DASHBOARD",domain1."/dashboard.php");
+
+
+?>
